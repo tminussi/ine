d31a3111af4d95c0bf45f84742562661da818ee8
Thales Minussi <tminussi@gmail.com>
2019-05-07 12:05:55 +0200

    .

--- http-handler/src/notifications/service/notification-service.js
+++ http-handler/src/notifications/service/notification-service.js
@@ -9,10 +9,10 @@ const SQS = new AWS.SQS()
 
 const put = async data => {
     console.log(data.level)
+    const caregiver = await dynamodb.fetchOne(data.source_id)
     if (data.level.toUpperCase() === 'PANIC') {
         console.log('sending message to... ', process.env.INE_QUEUE_URL)
         try {
-            const caregiver = await dynamodb.fetchOne(data.source_id)
             await SQS.sendMessage({
                 QueueUrl: process.env.INE_QUEUE_URL,
                 MessageBody: JSON.stringify(caregiver)
@@ -26,7 +26,7 @@ const put = async data => {
         })
     }
     data.id = Buffer.from(data.source_id, 'utf-8').toString('base64')
-    data.caregiver_details = await dynamodb.fetchOne(data.source_id)
+    data.caregiver_details = caregiver
     data.next_notification = new Date().getTime() + intervals[data.caregiver_details.interval]
     data.message = 'Thanks for letting us know you are OK.'
     return await dynamodb.put(data)
